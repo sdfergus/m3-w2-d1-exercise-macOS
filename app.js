fcfa7916bca1616eab6c58e78ee3e61dbaa3a056
Shandira <shandira.ferguson@gmail.com>
2022-11-03 21:08:18 -0700

    Collection customers displayed successfully

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var MongoClient = require( 'mongodb' ).MongoClient;
 
-//nodemongo is the ddatabase name
+//nodemongo is the database name
 var url = "mongodb://localhost:27017/nodemongo";
 
 //Connect to the db
 MongoClient.connect( url, {
   useNewUrlParser: true, useUnifiedTopology: true
 }, function ( err, db ) {
-  if ( err ) throw err;
-  console.log( "Database created!" );
-  db.close();
+  // if ( err ) throw err;
+  // console.log( "Database created!" );
+  // db.close();
+
+  var dbo = db.db( "nodemongo" );
+  dbo.createCollection( "customers", function ( err, res ) {
+    if ( err ) throw err;
+    console.log( "Collection created!" );
+    db.close();
+  } );
+
 } );
 
