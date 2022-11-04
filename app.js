c81596758708d255ef79aa12059cb857c662a282
Shandira <shandira.ferguson@gmail.com>
2022-11-03 21:26:53 -0700

    All 14 documents successfully displayed

--- app.js
+++ app.js
@@ -17,10 +17,34 @@ MongoClient.connect( url, {
   //   console.log( "Collection created!" );
   //   db.close();
   // } );
-  var custData = { name: "Westcliff Inc", address: "Irvine, CA" };
-  dbo.collection( "customers" ).insertOne( custData, function ( err, res ) {
+
+  // var custData = { name: "Westcliff Inc", address: "Irvine, CA" };
+  // dbo.collection( "customers" ).insertOne( custData, function ( err, res ) {
+  //   if ( err ) throw err;
+  //   console.log( "1 document inserted" );
+  //   db.close();
+  // } )
+
+  var custData = [
+    { name: 'John', address: 'Highway 71' },
+    { name: 'Peter', address: 'Lowstreet 4' },
+    { name: 'Amy', address: 'Apple st 652' },
+    { name: 'Hannah', address: 'Mountain 21' },
+    { name: 'Michael', address: 'Valley 345' },
+    { name: 'Sandy', address: 'Ocean blvd 2' },
+    { name: 'Betty', address: 'Green Grass 1' },
+    { name: 'Richard', address: 'Sky st 331' },
+    { name: 'Susan', address: 'One way 98' },
+    { name: 'Vicky', address: 'Yellow Garden 2' },
+    { name: 'Ben', address: 'Park Lane 38' },
+    { name: 'William', address: 'Central st 954' },
+    { name: 'Chuck', address: 'Main Road 989' },
+    { name: 'Viola', address: 'Sideway 1633' }
+  ];
+
+  dbo.collection( "customers" ).insertMany( custData, function ( err, res ) {
     if ( err ) throw err;
-    console.log( "1 document inserted" );
+    console.log( "Number of documents inserted: " + res.insertedCount );
     db.close();
   } )
 
